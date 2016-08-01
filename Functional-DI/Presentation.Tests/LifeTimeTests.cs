8fde9d1ad07bb332c302f52618acc9388a50c3a4
Etienne Pierrot <etienne.pierrot@mangopay.com>
2016-08-01 14:27:55 +0200

    test for Dispose LifeTime

--- Functional-DI/Presentation.Tests/LifeTimeTests.cs
+++ Functional-DI/Presentation.Tests/LifeTimeTests.cs
@@ -51,15 +51,56 @@ namespace Presentation.Tests
                 Assert.NotSame(repo1, repo2);
             }
         }
+
+        [Fact]
+        public void PerThreadShouldDisposeOncePerThread()
+        {
+            Func<FakeRepository> repositoryFactory = () => new FakeRepository();
+
+            var lifeTime = new LifeTime();
+            FakeRepository repo1 = null;
+            FakeRepository repo2 = null;
+
+            var t1 = new Thread(() =>
+            {
+                repo1 = lifeTime.PerThread(repositoryFactory);
+            });
+
+            var t2 = new Thread(() =>
+            {
+                repo2 = lifeTime.PerThread(repositoryFactory);
+            });
+
+            t1.Start();
+            t2.Start();
+
+            t1.Join();
+            t2.Join();
+
+            
+            lifeTime.Dispose();
+
+            Assert.True(repo1.IsDispose);
+            Assert.True(repo2.IsDispose);
+
+        }
     }
 
-    public class FakeRepository
+    public class FakeRepository : IDisposable
     {
+        public bool IsDispose { get; private set; }
+
         public FakeRepository()
         {
+            IsDispose = false;
             InitializedTimes++;
         }
 
         public int InitializedTimes { get; private set; }
+
+        public void Dispose()
+        {
+            IsDispose = true;
+        }
     }
 }
